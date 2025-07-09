20f931affb4488664826514adc4c80eb91a049d9
Pham Hung <hungk77777@gmail.com>
2025-07-09 17:56:12 +0700

    init

---
+++ client/src/assets/assets.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import marvelLogo from './marvel-logo.png';
+import googlePlay from './google-play.png';
+import appStore from './app-store.png';
+import screenImage from './screen-image.png';
+import profile from './profile.png';
+
+export const assets = {
+    logo,
+    marvelLogo,
+    googlePlay,
+    appStore,
+    screenImage,
+    profile
+}
+
+export const dummyTrailer = [
+
+]
